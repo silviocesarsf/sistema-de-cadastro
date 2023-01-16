c3e0b0f2f4bab4d62e4fb92fb5b34e3d6f7e54c8
Silvio César <117873185+silviocesarsf@users.noreply.github.com>
2023-01-15 23:04:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// Variaveis
+
+const modal = document.querySelector('.modal-cadastro')
+const bg = document.querySelector('.background-modal')
+const nomeLogin = document.querySelector('#nome')
+const cidadeLogin = document.querySelector('#cidade')
+const telLogin = document.querySelector('#tel')
+const idadeLogin = document.querySelector('#idade')
+const emailLogin = document.querySelector('#email')
+const nomeReg = document.querySelector('#nome-reg')
+const cidadeReg = document.querySelector('#cidade-reg')
+const telReg = document.querySelector('#tel-reg')
+const idadeReg = document.querySelector('#idade-reg')
+const emailReg = document.querySelector('#email-reg')
+const cadastrar = document.querySelector('#cad')
+
+document.querySelector('#btn-cadastrar').addEventListener('click', () => {
+    modal.classList.toggle('on')
+    bg.classList.remove('off')
+    document.querySelector('.btn-voltar').addEventListener('click', () => {
+        modal.classList.remove('on')
+        bg.classList.add('off')
+    })
+})
+
+cadastrar.addEventListener('click', () => {
+    modalCadastro()
+    modal.classList.remove('on')
+    bg.classList.add('off')
+})
+
+function modalCadastro() {
+    let tabelaCad = document.querySelector('.tabela')
+    let row = document.createElement('tr')
+
+    dadosReg = [ nomeReg.value, cidadeReg.value, telReg.value, idadeReg.value, emailReg.value ]
+
+    for (let i = 0; i < dadosReg.length; i++) {
+        let el = document.createElement('td')
+        row.appendChild(el)
+        tabelaCad.appendChild(row)
+        el.innerHTML = dadosReg[i]
+    }
+}
