7c06e126fb5d16fff0c05b48a2f27c82c05b5c3c
Silvio César <117873185+silviocesarsf@users.noreply.github.com>
2023-01-15 23:56:41 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const nomeLogin = document.querySelector('#nome')
 const cidadeLogin = document.querySelector('#cidade')
 const telLogin = document.querySelector('#tel')
 const idadeLogin = document.querySelector('#idade')
-const emailLogin = document.querySelector('#email')
 const nomeReg = document.querySelector('#nome-reg')
 const cidadeReg = document.querySelector('#cidade-reg')
 const telReg = document.querySelector('#tel-reg')
 const idadeReg = document.querySelector('#idade-reg')
-const emailReg = document.querySelector('#email-reg')
 const cadastrar = document.querySelector('#cad')
 
 document.querySelector('#btn-cadastrar').addEventListener('click', () => {
@@ -33,7 +31,7 @@ function modalCadastro() {
     let tabelaCad = document.querySelector('.tabela')
     let row = document.createElement('tr')
 
-    dadosReg = [ nomeReg.value, cidadeReg.value, telReg.value, idadeReg.value, emailReg.value ]
+    dadosReg = [ nomeReg.value, cidadeReg.value, idadeReg.value,telReg.value ]
 
     for (let i = 0; i < dadosReg.length; i++) {
         let el = document.createElement('td')
